c5eb2e00fd742e537f3c384c215cf7aff7037ec8
Shubhra <shubhi.srvstv@gmail.com>
2018-09-20 19:36:50 +0530

    HashTable Impl Using Chaining

--- src/main/java/com/programmergirl/HashTableUsingChaining.java
+++ src/main/java/com/programmergirl/HashTableUsingChaining.java
@@ -1,66 +1,104 @@
 package com.programmergirl;
 
-public class HashTableUsingChaining<T> {
+import java.util.*;
+
+public class HashTableUsingChaining<K,V> {
+
+    private HashEntry[] hashTable;
+    private int size;
     
-    public static final int SIZE = 1000;
-    private class Node<T> {
-        T data;
-        Node<T> next;
+    private class HashEntry<K, V> {
+        K key;
+        V value;
+        HashEntry<K, V> next;
         
-        Node(T data, Node<T> next) {
-            this.data = data;
-            this.next = next;
-        }
-        
-         Node(T data) {
-            this.data = data;
-            this.next = null;
+        HashEntry(K key, V value) {
+            this.key = key;
+            this.value = value;
         }
     }
-   private Node<T>[] hashTable = new Node[SIZE];
-    
-    public void insert(T data) {
-        int index = data.hashCode() % SIZE;
-        Node curr = hashTable[index];
-        if(curr == null) {
-            hashTable[index] = new Node(data);
-            return;
-        }
-        while(curr.next!=null)
-            curr = curr.next;
-        curr.next = new Node(data);
+
+    public HashTableUsingChaining(int capacity) {
+        this.hashTable = new HashEntry[capacity];
     }
-    
-    public boolean search(T data) {
-        int index = data.hashCode() % SIZE;
-        Node curr = hashTable[index];
-        if(curr == null)
-            return false;
-        if(curr.data.equals(data))
-            return true;
-        while(curr.next != null) {
-            if(curr.next.data.equals(data))
+
+    private int getElementIndex(K key) {
+        return key.hashCode() % hashTable.length;
+    }
+
+    public void put(K key, V value) {
+        if(key == null)
+            throw new IllegalArgumentException("Key can't be null");
+        int index = getElementIndex(key);
+        HashEntry<K,V> newEntry = new HashEntry(key, value);
+        if(hashTable[index] == null) {
+            hashTable[index] = newEntry;
+        } else {
+            System.out.println("Element Exists at index :"+index);
+            hashTable[index].next = newEntry;
+        }
+        size++;
+    }
+
+    public V get(K key) {
+        if(key == null)
+            throw new IllegalArgumentException("Key can't be null");
+        int index = getElementIndex(key);
+        HashEntry<K,V> current = hashTable[index];
+        while(current != null && key != current.key) {
+            current = current.next;
+        }
+        System.out.println(current.value);
+        return current.value;
+    }
+
+    public boolean containsKey(K key) {
+        int index = getElementIndex(key);
+        HashEntry<K,V> current = hashTable[index];
+        while(current != null) {
+            if(current.key == key)
                 return true;
         }
         return false;
-        
     }
-    
-    public void delete(T data) {
-        int index = data.hashCode() % SIZE;
-        Node curr = hashTable[index];
-        if(curr == null)
-            return;
-        if(curr.data.equals(data)) {
+
+    public V remove(K key) {
+        int index = getElementIndex(key);
+        HashEntry<K,V> current = hashTable[index];
+        HashEntry<K, V> hashEntryRemoved;
+        if(current == null)
+            throw new IllegalArgumentException("No such key exists!");
+        if(current.key == key) {
             hashTable[index] = null;
-            return;
+            hashEntryRemoved = current;
         }
-        while(curr.next != null && !curr.next.data.equals(data))
-            curr=curr.next;
-        if(curr.next != null)
-            curr.next = curr.next.next;
+        else {
+            while(current.next != null && current.next.key != key) {
+                current = current.next;
+            }    
+            if(current.next == null)
+                throw new IllegalArgumentException("No such key exists!");
+            hashEntryRemoved = current.next;
+            current.next = current.next.next;
+        }
+        size--;
+        return hashEntryRemoved.value;
     }
     
-    
-    
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        String result = "[";
+        for(HashEntry<K, V> entry : hashTable) {
+            while(entry != null) {
+                result += "{"+ entry.key + ", " + entry.value + "}";
+                entry = entry.next;
+            }
+        }
+        result += "]";
+        return result;
+    }
 }
--- src/test/java/com/programmergirl/HashTableUsingChainingTest.java
+++ src/test/java/com/programmergirl/HashTableUsingChainingTest.java
@@ -2,28 +2,51 @@ package com.programmergirl;
 
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+import org.junit.Before;
 
 public class HashTableUsingChainingTest 
 {
-   
-    @Test
-    public void testHashTable()
-    {
-       HashTableUsingChaining<String> hashTable = new HashTableUsingChaining();
-       hashTable.insert("Apple");
-       hashTable.insert("Banana");
-       hashTable.insert("Guava");
 
-       assertFalse(hashTable.search("Grapes"));
-       assertTrue(hashTable.search("Banana"));
-       assertTrue(hashTable.search("Apple"));
-       assertTrue(hashTable.search("Guava"));
-       assertFalse(hashTable.search("guava"));
+	private HashTableUsingChaining<String, String> hashTable = new HashTableUsingChaining(10);
 
-       hashTable.delete("Guava");
+	@Before
+	public void init() {
+		hashTable.put("1", "Apple");
+		hashTable.put("2", "Banana");
+		hashTable.put("3", "Guava");
+	}
 
-       assertFalse(hashTable.search("Guava")); 
-    }
+	@Test
+	public void givenHashTable_whenGetInvokedWithKey_thenReturnsElementValue()
+	{
+		assertEquals(hashTable.get("2"), "Banana");
+		assertEquals(hashTable.get("1"), "Apple");
+		assertEquals(hashTable.get("3"), "Guava");
+
+	}
+
+	@Test
+	public void givenHashTable_whenContainsKeyInvoked_thenChecksElementExistsOrNot()
+	{
+		assertTrue(hashTable.containsKey("1"));
+		assertTrue(hashTable.containsKey("2"));
+		assertTrue(hashTable.containsKey("3"));
+		assertFalse(hashTable.containsKey("4"));
+	}
+
+	@Test
+	public void givenHashTable_whenSizeInvoked_thenReturnSize()
+	{
+		assertEquals(hashTable.size(), 3);
+	}
+
+	@Test
+	public void givenHashTable_whenRemoveInvoked_thenRemoveElementAndReturnItsValue()
+	{
+		assertEquals(hashTable.remove("2"), "Banana");
+		assertEquals(hashTable.size(), 2);    
+	}
 }
